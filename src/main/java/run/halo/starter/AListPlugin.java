ce05bcb1341c50e5f19ec78baf80b1615ee4b54f
roozen <2273038475@qq.com>
2024-07-03 00:43:54 +0800

    Inital commit

---
+++ src/main/java/run/halo/starter/AListPlugin.java
@@ -0,0 +1,36 @@
+package run.halo.starter;
+
+import org.springframework.stereotype.Component;
+import run.halo.app.plugin.BasePlugin;
+import run.halo.app.plugin.PluginContext;
+
+/**
+ * <p>Plugin main class to manage the lifecycle of the plugin.</p>
+ * <p>This class must be public and have a public constructor.</p>
+ * <p>Only one main class extending {@link BasePlugin} is allowed per plugin.</p>
+ *
+ * @author guqing
+ * @since 1.0.0
+ */
+@Component
+public class AListPlugin extends BasePlugin {
+
+    public AListPlugin(PluginContext pluginContext) {
+        super(pluginContext);
+    }
+
+    @Override
+    public void start() {
+        System.out.println("插件启动成功！");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("插件停止！");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("插件被删除！");
+    }
+}
