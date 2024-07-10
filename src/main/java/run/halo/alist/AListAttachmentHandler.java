2a5832eec43a5faf3df1f97c2cc87810b8562761
roozen <2273038475@qq.com>
2024-07-10 22:20:28 +0800

    feat: token添加缓存过期时间

--- src/main/java/run/halo/alist/AListAttachmentHandler.java
+++ src/main/java/run/halo/alist/AListAttachmentHandler.java
@@ -1,12 +1,15 @@
 package run.halo.alist;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.pf4j.Extension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +32,7 @@ import run.halo.app.extension.Secret;
 import run.halo.app.infra.utils.JsonUtils;
 
 /**
- * 文件描述
+ * AListAttachmentHandler
  *
  * @author： <a href="https://roozen.top">Roozen</a>
  * @date: 2024/7/3
@@ -44,90 +47,57 @@ public class AListAttachmentHandler implements AttachmentHandler {
 
     WebClient webClient = null;
     AListProperties properties = null;
-    String token = "";
+
+    private final Cache<String, String> tokenCache = Caffeine.newBuilder()
+        .expireAfterWrite(1, TimeUnit.DAYS)
+        .build();
 
     @Override
     public Mono<Attachment> upload(UploadContext uploadContext) {
         return Mono.just(uploadContext).filter(context -> this.shouldHandle(context.policy()))
-            .flatMap(context -> {
-                properties = getProperties(context.configMap());
-                if (webClient == null) {
-                    webClient = WebClient.builder()
-                        .baseUrl(properties.getSite())
-                        .build();
-                }
-
-                var secretName = properties.getSecretName();
-                return client.fetch(Secret.class, secretName)
-                    .switchIfEmpty(Mono.error(new IllegalArgumentException(
-                        "Secret " + secretName + " not found")))
-                    .flatMap(secret -> {
-                        var stringData = secret.getStringData();
-                        var usernameKey = "username";
-                        var passwordKey = "password";
-                        if (stringData == null
-                            || !(stringData.containsKey(usernameKey) && stringData.containsKey(
-                            passwordKey))) {
-                            return Mono.error(new IllegalArgumentException(
-                                "Secret " + secretName
-                                    + " does not have username or password key"));
-                        }
-                        var username = stringData.get(usernameKey);
-                        var password = stringData.get(passwordKey);
-                        return webClient.post()
-                            .uri("/api/auth/login")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .body(Mono.just(
-                                AListLoginReq.builder()
-                                    .username(username)
-                                    .password(password)
-                                    .build()), AListLoginReq.class)
-                            .retrieve()
-                            .bodyToMono(
-                                new ParameterizedTypeReference<AListResult<AListLoginRes>>() {
-                                });
-                    });
-            }).flatMap(response -> {
-                if (response.getCode().equals("200")) {
-                    log.info("AList 登录成功");
-                    this.token = response.getData().getToken();
-                }
-                return webClient.put()
-                    .uri("/api/fs/put")
-                    .header("Authorization", token)
-                    .header("File-Path", URLEncoder.encode(
-                        properties.getPath() + "/" + uploadContext.file().name(),
-                        StandardCharsets.UTF_8))
-                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                    .body(uploadContext.file().content().cache(), DataBuffer.class)
-                    .retrieve()
-                    .bodyToMono(
-                        new ParameterizedTypeReference<AListResult<String>>() {
-                        });
-            })
-            .flatMap(response -> {
-                if (response.getCode().equals("200")) {
-                    log.info("AList 上传成功");
-                }
-                return webClient.post()
-                    .uri("/api/fs/get")
-                    .header("Authorization", token)
-                    .body(Mono.just(
-                            AListGetFileInfoReq
-                                .builder()
-                                .path(properties.getPath() + "/" + uploadContext.file().name())
-                                .build()),
-                        AListGetFileInfoReq.class)
-                    .retrieve()
-                    .bodyToMono(
-                        new ParameterizedTypeReference<AListResult<AListGetFileInfoRes>>() {
-                        });
-            })
+            .map(UploadContext::configMap)
+            .flatMap(this::auth)
+            .flatMap(token -> webClient.put()
+                .uri("/api/fs/put")
+                .header("Authorization", token)
+                .header("File-Path", UriUtils.encodePath(
+                    properties.getPath() + "/" + uploadContext.file().name(),
+                    StandardCharsets.UTF_8))
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(uploadContext.file().content().cache(), DataBuffer.class)
+                .retrieve()
+                .bodyToMono(
+                    new ParameterizedTypeReference<AListResult<String>>() {
+                    })
+                .flatMap(response -> {
+                    if (response.getCode().equals("200")) {
+                        log.info("AList 上传文件{}成功", uploadContext.file().name());
+                        return Mono.just(token);
+                    }
+                    return Mono.error(new RuntimeException(response.getMessage()));
+                })
+            )
+            .flatMap(token -> webClient.post()
+                .uri("/api/fs/get")
+                .header("Authorization", token)
+                .body(Mono.just(
+                        AListGetFileInfoReq
+                            .builder()
+                            .path(properties.getPath() + "/" + uploadContext.file().name())
+                            .build()),
+                    AListGetFileInfoReq.class)
+                .retrieve()
+                .bodyToMono(
+                    new ParameterizedTypeReference<AListResult<AListGetFileInfoRes>>() {
+                    })
+                .flatMap(response -> {
+                    if (response.getCode().equals("200")) {
+                        log.info("AList 获取文件{}成功", uploadContext.file().name());
+                        return Mono.just(response);
+                    }
+                    return Mono.error(new RuntimeException(response.getMessage()));
+                }))
             .map(response -> {
-                if (response.getCode().equals("200")) {
-                    log.info("AList 上传成功");
-                }
-                log.info(response.getData().toString());
                 var metadata = new Metadata();
                 metadata.setName(UUID.randomUUID().toString());
                 metadata.setAnnotations(
@@ -149,6 +119,61 @@ public class AListAttachmentHandler implements AttachmentHandler {
             });
     }
 
+    private Mono<String> auth(ConfigMap configMap) {
+        if (properties == null) {
+            properties = getProperties(configMap);
+        }
+        if (webClient == null) {
+            webClient = WebClient.builder()
+                .baseUrl(properties.getSite())
+                .build();
+        }
+
+        String secretName = properties.getSecretName();
+        if (tokenCache.getIfPresent(secretName) != null) {
+            return Mono.just(
+                Objects.requireNonNull(tokenCache.getIfPresent(secretName)));
+        }
+
+        return client.fetch(Secret.class, secretName)
+            .switchIfEmpty(Mono.error(new IllegalArgumentException(
+                "Secret " + secretName + " not found")))
+            .flatMap(secret -> {
+                var stringData = secret.getStringData();
+                var usernameKey = "username";
+                var passwordKey = "password";
+                if (stringData == null
+                    || !(stringData.containsKey(usernameKey) && stringData.containsKey(
+                    passwordKey))) {
+                    return Mono.error(new IllegalArgumentException(
+                        "Secret " + secretName
+                            + " does not have username or password key"));
+                }
+                var username = stringData.get(usernameKey);
+                var password = stringData.get(passwordKey);
+                return webClient.post()
+                    .uri("/api/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Mono.just(
+                        AListLoginReq.builder()
+                            .username(username)
+                            .password(password)
+                            .build()), AListLoginReq.class)
+                    .retrieve()
+                    .bodyToMono(
+                        new ParameterizedTypeReference<AListResult<AListLoginRes>>() {
+                        });
+            }).flatMap(response -> {
+                if (response.getCode().equals("200")) {
+                    log.info("AList 登录成功");
+                    return Mono.just(
+                        tokenCache.get(secretName, k -> response.getData().getToken()));
+                }
+                return Mono.error(new IllegalArgumentException(
+                    "wrong Username Or Password"));
+            });
+    }
+
     private AListProperties getProperties(ConfigMap configMap) {
         var settingJson = configMap.getData().getOrDefault("default", "{}");
         return JsonUtils.jsonToObject(settingJson, AListProperties.class);
@@ -157,85 +182,74 @@ public class AListAttachmentHandler implements AttachmentHandler {
     @Override
     public Mono<Attachment> delete(DeleteContext deleteContext) {
         return Mono.just(deleteContext).filter(context -> this.shouldHandle(context.policy()))
-            .flatMap(context -> {
-                    properties = getProperties(context.configMap());
-                    if (webClient == null) {
-                        webClient = WebClient.builder()
-                            .baseUrl(properties.getSite())
-                            .build();
+            .map(DeleteContext::configMap)
+            .flatMap(this::auth)
+            .flatMap(token -> webClient.post()
+                .uri("/api/fs/remove")
+                .header("Authorization", token)
+                .body(Mono.just(AListRemoveFileReq.builder()
+                    .dir(properties.getPath())
+                    .names(List.of(deleteContext.attachment().getSpec().getDisplayName()))
+                    .build()), AListGetFileInfoReq.class)
+                .retrieve()
+                .bodyToMono(
+                    new ParameterizedTypeReference<AListResult<String>>() {
+                    })
+                .flatMap(response -> {
+                    if (response.getCode().equals("200")) {
+                        log.info("AList 删除文件{}成功",
+                            deleteContext.attachment().getSpec().getDisplayName());
+                        return Mono.just(token);
                     }
-                    return webClient.post()
-                        .uri("/api/fs/remove")
-                        .header("Authorization", token)
-                        .body(Mono.just(AListRemoveFileReq.builder()
-                            .dir(properties.getPath())
-                            .names(List.of(deleteContext.attachment().getSpec().getDisplayName()))
-                            .build()), AListGetFileInfoReq.class)
-                        .retrieve()
-                        .bodyToMono(
-                            new ParameterizedTypeReference<AListResult<String>>() {
-                            });
-                }
+                    return Mono.error(new RuntimeException(response.getMessage()));
+                })
             )
-            .map(response -> {
-                if (response.getCode().equals("200")) {
-                    log.info("AList 删除成功");
-                }
-                return deleteContext.attachment();
-            });
+            .map(token -> deleteContext.attachment());
     }
 
     @Override
     public Mono<URI> getSharedURL(Attachment attachment, Policy policy, ConfigMap configMap,
         Duration ttl) {
-        if (!this.shouldHandle(policy)) {
-            return Mono.empty();
-        }
-        return webClient.post()
-            .uri("/api/fs/get")
-            .header("Authorization", token)
-            .body(Mono.just(
-                    AListGetFileInfoReq
-                        .builder()
-                        .path(properties.getPath() + "/" + attachment.getSpec().getDisplayName())
-                        .build()),
-                AListGetFileInfoReq.class)
-            .retrieve()
-            .bodyToMono(
-                new ParameterizedTypeReference<AListResult<AListGetFileInfoRes>>() {
-                })
-            .map(response -> URI.create(UriUtils.encodePath(
-                properties.getSite() + "/d" + properties.getPath() + "/"
-                    + response.getData().getName(),
-                StandardCharsets.UTF_8)));
+        return Mono.just(policy).filter(this::shouldHandle).flatMap(p -> auth(configMap))
+            .flatMap(token -> webClient.post()
+                .uri("/api/fs/get")
+                .header("Authorization", tokenCache.getIfPresent(properties.getSecretName()))
+                .body(Mono.just(
+                        AListGetFileInfoReq
+                            .builder()
+                            .path(properties.getPath() + "/" + attachment.getSpec().getDisplayName())
+                            .build()),
+                    AListGetFileInfoReq.class)
+                .retrieve()
+                .bodyToMono(
+                    new ParameterizedTypeReference<AListResult<AListGetFileInfoRes>>() {
+                    })
+                .map(response -> URI.create(UriUtils.encodePath(
+                    properties.getSite() + "/d" + properties.getPath() + "/"
+                        + response.getData().getName(),
+                    StandardCharsets.UTF_8))));
     }
 
     @Override
     public Mono<URI> getPermalink(Attachment attachment, Policy policy, ConfigMap configMap) {
-        if (!this.shouldHandle(policy)) {
-            return Mono.empty();
-        }
-        return webClient.post()
-            .uri("/api/fs/get")
-            .header("Authorization", token)
-            .body(Mono.just(
-                    AListGetFileInfoReq
-                        .builder()
-                        .path(properties.getPath() + "/" + attachment.getSpec().getDisplayName())
-                        .build()),
-                AListGetFileInfoReq.class)
-            .retrieve()
-            .bodyToMono(
-                new ParameterizedTypeReference<AListResult<AListGetFileInfoRes>>() {
-                })
-            .map(response -> {
-                URI uri = URI.create(UriUtils.encodePath(
+        return Mono.just(policy).filter(this::shouldHandle).flatMap(p -> auth(configMap))
+            .flatMap(token -> webClient.post()
+                .uri("/api/fs/get")
+                .header("Authorization", tokenCache.getIfPresent(properties.getSecretName()))
+                .body(Mono.just(
+                        AListGetFileInfoReq
+                            .builder()
+                            .path(properties.getPath() + "/" + attachment.getSpec().getDisplayName())
+                            .build()),
+                    AListGetFileInfoReq.class)
+                .retrieve()
+                .bodyToMono(
+                    new ParameterizedTypeReference<AListResult<AListGetFileInfoRes>>() {
+                    })
+                .map(response -> URI.create(UriUtils.encodePath(
                     properties.getSite() + "/d" + properties.getPath() + "/"
                         + response.getData().getName(),
-                    StandardCharsets.UTF_8));
-                log.info("alist getPermalink:" + uri.toString());
-                return uri;
-            });
+                    StandardCharsets.UTF_8))));
     }
 
     boolean shouldHandle(Policy policy) {
